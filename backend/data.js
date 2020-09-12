a810afa7ed0b06c161f4b206ad14085ec0f3cea2
Quan Le <quanle@Quans-MBP.lan>
2020-09-12 14:11:39 -0400

    init

---
+++ backend/data.js
@@ -0,0 +1,44 @@
+export default {
+    products:[
+        {
+        _id: '1',
+        name: ' Shirt',
+        category: 'Shirts',
+        image: '/images/d1.jpg',
+        price: 59.99,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10
+        },
+        {   
+            _id: '2',
+            name: 'Bot Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 59.99,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10
+        },
+        {
+            _id: '3',
+            name: 'Best Pant',
+            category: 'Pants',
+            image: '/images/d2.png',
+            price: 69.99,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10
+        },
+        {
+            _id: '4',
+            name: 'good Pant',
+            category: 'Pants',
+            image: '/images/d2.png',
+            price: 69.99,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10
+        },
+    ]
+}
